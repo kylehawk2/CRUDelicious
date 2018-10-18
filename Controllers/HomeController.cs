de7f18a323aa8e7eaadd2945854ebfdb9a773f60
Kyle Hawkins <kylehawk2@gmail.com>
2018-10-18 11:07:37 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CRUDelicious.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDelicious.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CRUDContext dbContext;
+
+        public HomeController (CRUDContext context)
+        {
+            dbContext = context;
+        }
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            List<Meal> AllDishes = dbContext.Dishes.ToList();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/CRUDContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDelicious.Models
+{
+    public class CRUDContext : DbContext
+    {
+        public CRUDContext(DbContextOptions<CRUDContext> options) : base(options) { }
+        public DbSet<Meal> Dishes {get;set;}
+    }
+}
